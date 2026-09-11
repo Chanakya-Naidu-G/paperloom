8f63a5335cb18cf21ba175b1b3eada28d20b04ae
Gorrepati Chanakya Naidu <chanakyagorrrepati@gmail.com>
2026-09-12 01:06:39 +0530

    fix: use stable AWS elastic IP

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "http://13.204.45.15:8000/api/v1/:path*",
+        destination: "http://13.207.50.207:8000/api/v1/:path*",
       },
     ];
   },
